bfe642ab294026e420a9947f37d0b517e430e755
mvraa <40834090+mvraa@users.noreply.github.com>
2020-10-14 12:32:35 +0200

    lavet til en klasse

--- GridderWebApplication/Gitter/wwwroot/js/canvas.js
+++ GridderWebApplication/Gitter/wwwroot/js/canvas.js
@@ -1,55 +1,42 @@
-﻿window.addEventListener('load', () => {
-    console.log("hello");
-});
+﻿class Gitter {
+    //constructor
+    constructor(currentColor = "black") {
+        this.canvas = document.querySelector("#canvas");
+        this.ctx = canvas.getContext("2d");
+        this.canvasPos = canvas.getBoundingClientRect();
 
+        this.resize(window.innerWidth, window.innerHeight)
 
+        window.addEventListener('click', this.placePixel.bind(this), false);
+    }
+
+    placePixel(e) {
+        this.ctx.fillStyle = "red";
+
+        this.ctx.fillRect(e.pageX - canvas.offsetLeft - 3, e.pageY - canvas.offsetTop - 3, 3, 3);
+    }
+
+    resize(x, y) {
+        this.canvas.height = y;
+        this.canvas.width = x;
+    }
+
+    test() {
+        this.ctx.strokeStyle = "red";
+        this.ctx.strokeRect(20, 20, 50, 80);
+
+        this.ctx.strokeRect(20, 20, 100, 60);
+
+        this.ctx.fillRect(6, 6, 1, 1);
+    }
+
+    setColor(color) {
+    }
+}
 
 
 window.addEventListener("load", () => {
-    var canvas = document.querySelector("#canvas");
-    var ctx = canvas.getContext("2d");
-    var canvasPos = canvas.getBoundingClientRect();
+    var gitter = new Gitter();
 
-
-    // Resize
-    canvas.height = window.innerHeight;
-    canvas.width = window.innerWidth;
-
-
-    ctx.strokeStyle = "red";
-    ctx.strokeRect(20, 20, 50, 80);
-
-    ctx.strokeRect(20, 20, 100, 60);
-
-    ctx.fillRect(6, 6, 1, 1);
-
-
-    function placePixel(e) {
-        ctx.fillStyle = "red";
-        console.log(e.clientX);
-        console.log("hej");
-       
-        ctx.fillRect(e.pageX - canvas.offsetLeft -3, e.pageY - canvas.offsetTop -3, 3, 3);
-
-    }
-
-
-
-    window.addEventListener("mousemove", function (e) {
-        console.log("mouse x, y:")
-        console.log(e.clientX);
-        console.log(e.clientY);
-
-        console.log("canvas x, y:")
-        console.log(canvasPos.left);
-        console.log(canvasPos.top);
-
-        console.log("page x, y:")
-        console.log(e.pageX);
-        console.log(e.pageY);
-    });
-
-
-    canvas.addEventListener("mousedown", placePixel);
-}
-);
+    gitter.test();
+});
