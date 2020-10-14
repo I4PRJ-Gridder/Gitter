d8bfaaf9fbaf0f2dd6b601e43d52d8523409239a
MagmaMusen <MagmaMusen@gmail.com>
2020-10-14 11:43:49 +0200

    Added visual studio prototype project

---
+++ GridderWebApplication/Gitter/wwwroot/js/canvas.js
@@ -0,0 +1,56 @@
+﻿window.addEventListener('load', () => {
+    console.log("hello");
+});
+
+
+
+
+window.addEventListener("load", () => {
+    var canvas = document.querySelector("#canvas");
+    var ctx = canvas.getContext("2d");
+    var canvasPos = canvas.getBoundingClientRect();
+
+
+    // Resize
+    canvas.height = window.innerHeight;
+    canvas.width = window.innerWidth;
+    canvas.style.top = 50px;
+
+
+    ctx.strokeStyle = "red";
+    ctx.strokeRect(20, 20, 50, 80);
+
+    ctx.strokeRect(20, 20, 100, 60);
+
+    ctx.fillRect(6, 6, 1, 1);
+
+
+    function placePixel(e) {
+        ctx.fillStyle = "red";
+        console.log(e.clientX);
+        console.log("hej");
+       
+        ctx.fillRect(e.pageX - canvasPos.left, e.pageY - canvasPos.top, 3, 3);
+
+    }
+
+
+
+    window.addEventListener("mousemove", function (e) {
+        console.log("mouse x, y:")
+        console.log(e.clientX);
+        console.log(e.clientY);
+
+        console.log("canvas x, y:")
+        console.log(canvasPos.left);
+        console.log(canvasPos.top);
+
+        console.log("page x, y:")
+        console.log(e.pageX);
+        console.log(e.pageY);
+    });
+
+
+    canvas.addEventListener("mousedown", placePixel);
+}
+);
